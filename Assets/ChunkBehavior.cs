f631d7cf607c32cf7e4802117489b40cb4419f11
Bruno Peres <dev.brunoperes@gmail.com>
2023-06-07 16:25:11 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/ChunkBehavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkBehavior : MonoBehaviour
+{
+    private const int CHUNK_SIZE = 10;
+    private const int SURFACE_HEIGHT = 2;
+
+    private List<GameObject> _voxels = new List<GameObject>();
+    private GameObject[] _voxelPrefabs;
+
+    void Start()
+    {
+        _voxelPrefabs = Resources.LoadAll<GameObject>("Voxels/");
+
+        for (int yAxis = 0; yAxis < SURFACE_HEIGHT; yAxis++)
+        {
+            for (int zAxis = 0; zAxis < CHUNK_SIZE; zAxis++)
+            {
+                for (int xAxis = 0; xAxis < CHUNK_SIZE; xAxis++)
+                {
+                    _voxels.Add(Instantiate(_voxelPrefabs[0], new Vector3(xAxis, yAxis, zAxis), Quaternion.identity, transform));
+                }
+            }
+        }
+    }
+}
